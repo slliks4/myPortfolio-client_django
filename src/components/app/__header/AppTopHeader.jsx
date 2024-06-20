d5b887605bf898be3e7762ada5abd99c18ee1938
skills nwokolo anthony <skillsnwokolo372@gmail.com>
2024-06-20 03:22:22 -0230

    added bg-color to the top header

--- src/components/app/__header/AppTopHeader.jsx
+++ src/components/app/__header/AppTopHeader.jsx
@@ -4,7 +4,7 @@ import MenuBtn from "../../MenuBtn";
 // Default Function
 export default function AppTopHeader() {
   return (
-    <header className="p-4 flex justify-between items-center text-4xl w-screen fixed z-10 bg-blur lg:hidden">
+    <header className="bg-gray-900 p-4 flex justify-between items-center text-4xl w-screen fixed z-10 lg:hidden">
         <h1 className="pr-1 ">
             <span className="text-primary font-semibold">S</span>lliks<span className=''>4</span>
         </h1>
